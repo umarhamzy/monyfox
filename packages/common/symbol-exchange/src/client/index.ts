9f283792d52e53fb39108639745fd74553cedb8e
LucaDiba <l.diba@protonmail.com>
2025-04-12 00:22:07 -0700

    Add symbol exchange package with Frankfurter

---
+++ packages/common/symbol-exchange/src/client/index.ts
@@ -0,0 +1,49 @@
+export type GetExchangeRateRequest = {
+  /**
+   * The symbol of the currency to convert from.
+   */
+  from: string;
+  /**
+   * The symbol of the currency to convert to.
+   */
+  to: string;
+  /**
+   * The first date of the range to get the exchange rate for.
+   * Format: YYYY-MM-DD.
+   */
+  startDate: string;
+  /**
+   * The last date of the range to get the exchange rate for.
+   * Format: YYYY-MM-DD.
+   */
+  endDate: string;
+};
+
+export type GetExchangeRateResponse = Array<{
+  /**
+   * The date of the exchange rate.
+   * Format: YYYY-MM-DD.
+   */
+  date: string;
+  /**
+   * The exchange rate.
+   */
+  rate: number;
+  /**
+   * Whether the exchange rate is final. If false, the exchange rate may change
+   * in the future for the given date.
+   */
+  isFinal: boolean;
+}>;
+
+export interface SymbolExchangeClient {
+  /**
+   * Get the exchange rates for a given symbol pair and date range.
+   * The response is an array of exchange rates, one for each existing date in the
+   * range. Some dates may be missing if the exchange rate is not available for
+   * that date.
+   */
+  getExchangeRates: (
+    params: GetExchangeRateRequest,
+  ) => Promise<GetExchangeRateResponse>;
+}
--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,3 +1,3 @@
 import { defineWorkspace } from "vitest/config";
 
-export default defineWorkspace(["apps/client/*"]);
+export default defineWorkspace(["apps/client/*", "packages/*/*"]);
