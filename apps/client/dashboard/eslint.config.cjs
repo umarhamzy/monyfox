f11db1cb55e2856223669379e67ca25d43777b5c
LucaDiba <l.diba@protonmail.com>
2025-08-30 19:33:12 -0700

    Bump eslint (ctnd)

--- apps/client/dashboard/eslint.config.cjs
+++ apps/client/dashboard/eslint.config.cjs
@@ -1,51 +1,48 @@
-const {
-    defineConfig,
-    globalIgnores,
-} = require("eslint/config");
-
-const globals = require("globals");
-
-const {
-    fixupConfigRules,
-} = require("@eslint/compat");
-
-const tsParser = require("@typescript-eslint/parser");
-const reactRefresh = require("eslint-plugin-react-refresh");
-const js = require("@eslint/js");
-
-const {
-    FlatCompat,
-} = require("@eslint/eslintrc");
+import { defineConfig, globalIgnores } from "eslint/config";
+import globals from "globals";
+import { fixupConfigRules } from "@eslint/compat";
+import tsParser from "@typescript-eslint/parser";
+import reactRefresh from "eslint-plugin-react-refresh";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
 
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-module.exports = defineConfig([{
+module.exports = defineConfig([
+  {
     languageOptions: {
-        globals: {
-            ...globals.browser,
-        },
+      globals: {
+        ...globals.browser,
+      },
 
-        parser: tsParser,
+      parser: tsParser,
     },
 
-    extends: fixupConfigRules(compat.extends(
+    extends: fixupConfigRules(
+      compat.extends(
         "eslint:recommended",
         "plugin:@typescript-eslint/recommended",
         "plugin:react-hooks/recommended",
         "plugin:@tanstack/query/recommended",
-    )),
+      ),
+    ),
 
     plugins: {
-        "react-refresh": reactRefresh,
+      "react-refresh": reactRefresh,
     },
 
     rules: {
-        "react-refresh/only-export-components": ["warn", {
-            allowConstantExport: true,
-        }],
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": ["error"],
     },
-}, globalIgnores(["**/dist", "**/.eslintrc.cjs"])]);
+  },
+  globalIgnores(["**/dist", "**/.eslintrc.cjs"]),
+]);
