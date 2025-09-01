60d52175553577b85307a2ca4797b8b4746965c5
LucaDiba <l.diba@protonmail.com>
2025-08-31 17:43:14 -0700

    Fix Plausible script

--- apps/client/dashboard/src/utils/analytics.ts
+++ apps/client/dashboard/src/utils/analytics.ts
@@ -1,16 +1,21 @@
-import { init } from "@plausible-analytics/tracker/plausible.js";
+import type { PluginOption } from "vite";
 import { z } from "zod";
 
-export function getAnalyticsConfig() {
-  const plausibleDomain = z
-    .string()
-    .url()
-    .safeParse(import.meta.env.VITE_PLAUSIBLE_DOMAIN);
+export function getAnalyticsConfig({
+  domain,
+  scriptUrl,
+}: {
+  domain: unknown;
+  scriptUrl: unknown;
+}) {
+  const plausibleDomain = z.string().min(1).safeParse(domain);
+  const plausibleScript = z.url().safeParse(scriptUrl);
 
-  if (plausibleDomain.success) {
+  if (plausibleDomain.success && plausibleScript.success) {
     return {
       isConfigured: true as const,
       domain: plausibleDomain.data,
+      scriptUrl: plausibleScript.data,
     };
   }
 
@@ -19,12 +24,32 @@ export function getAnalyticsConfig() {
   };
 }
 
-export function initializeAnalytics() {
-  const config = getAnalyticsConfig();
+export function analyticsPlugin(): PluginOption {
+  return {
+    name: "analytics",
+    transformIndexHtml(html) {
+      // Environment variables are not available here, having them into the
+      // index.html file and parsing them from here is a workaround
+      let script = "";
 
-  if (config.isConfigured) {
-    init({
-      domain: config.domain,
-    });
-  }
+      const regex =
+        /<script[\s\S]*?id="analytics"[\s\S]*?data-domain="([^"]*)"[\s\S]*?src="([^"]*)"[\s\S]*?>[\s\S]*?<\/script>/;
+      const match = html.match(regex);
+
+      if (match) {
+        const config = getAnalyticsConfig({
+          domain: match[1],
+          scriptUrl: match[2],
+        });
+
+        if (config.isConfigured) {
+          script = match[0]
+            .replace(/data-domain="[^"]*"/, `data-domain="${config.domain}"`)
+            .replace(/src="[^"]*"/, `src="${config.scriptUrl}"`);
+        }
+      }
+
+      return html.replace(regex, script);
+    },
+  };
 }
